b7dd92945cf7197df19db5175cd89f6425dab9b2
Your Name <user@example.com>
2025-03-08 22:47:00 +0100

    Update website: change admin password, modify header/footer, update Impressum

--- reset_password.py
+++ reset_password.py
@@ -4,8 +4,8 @@ from simple_app import app
 with app.app_context():
     user = User.query.filter_by(username='admin').first()
     if user:
-        user.set_password('admin')
+        user.set_password('Elegant2000?')
         db.session.commit()
-        print('Passwort erfolgreich auf "admin" aktualisiert!')
+        print('Passwort erfolgreich auf "Elegant2000?" aktualisiert!')
     else:
         print('Admin-Benutzer nicht gefunden!') 
